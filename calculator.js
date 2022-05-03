769c6de384737dab7bbf3bb9bc6d81ab1915ee42
lucasbotm <lucasbotm@gmail.com>
2022-05-03 20:05:39 +0200

    add calculator structure in html, flex box structure in CSS and base functions JS

---
+++ calculator.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b 
+}
+
+function operate(a,b,operation){
+    switch (operation){
+        case "add":
+            result = add(a,b);
+            break;
+        case "subtract":
+            result = subtract(a,b);
+            break;
+        case "multiply":
+            result = multiply(a,b);
+            break;
+        case "divide":
+            result = divide(a,b);
+            break;
+    }
+}

c41a8177843f9e288e6260898b7c39cdc25ea7be
lucasbotm <lucasbotm@gmail.com>
2022-05-09 20:43:34 +0200

    add operations to display, calculations still returns NaN

--- calculator.js
+++ calculator.js
@@ -17,33 +17,78 @@ function divide(a,b){
 }
 
 function operate(a,b,operation){
+    let result = null;
     switch (operation){
         case "add":
             result = add(a,b);
+            return result;
             break;
         case "subtract":
             result = subtract(a,b);
+            return result;
             break;
         case "multiply":
             result = multiply(a,b);
+            return result;
             break;
         case "divide":
             result = divide(a,b);
+            return result;
             break;
     }
 }
 
 function populateDisplay(input){
-    if (display.innerHTML===""){
-        display.innerHTML = input
+    if (currentOperation === null){
+        if (Object.keys(operations).includes(input)){
+            if (display.innerHTML === ""){
+                return
+            }
+            currentResult = +display.innerHTML;
+            currentOperation = operations[input];
+            display.innerHTML = "";
+        }
+
+        else if (display.innerHTML===""){
+            display.innerHTML = input
+        }
+        else{
+            display.innerHTML += input
+        }
     }
-    else{
-        display.innerHTML += input
+    else {
+        if (Object.keys(operations).includes(input)){
+            console.log(currentResult)
+            input = + input
+
+            currentResult = operate(currentResult,input,currentOperation);
+            display.innerHTML = currentResult;
+            console.log(currentResult)
+            currentOperation = null;
+        }
+
+        else if (display.innerHTML===""){
+            display.innerHTML = input
+        }
+        else{
+            display.innerHTML += input
+        }
     }
 }
 
 let display = document.querySelector(".display");
 display.innerHTML = ""
+
+let operations = {
+    "+": "add",
+    "-": "subtract",
+    "*": "multiply",
+    "/": "divide",
+    "=": "equals"
+};
+
+let currentResult = null;
+let currentOperation = null;
 
 document.querySelectorAll(".button").forEach(button =>{
     button.addEventListener("click", e=>{
